2017fe4442c9aed8035ebe712848b7f37ba8befe
Murat Han Kocaman <murathankocaman@outlook.com>
2024-12-28 20:21:41 +0300

    q

--- server/controllers/badge.js
+++ server/controllers/badge.js
@@ -1,4 +1,4 @@
-const Badge = require('../models/Badge');
+const FileUpload = require('../models/FileUpload'); // FileUpload modelini dahil edin
 
 // Yeni rozet oluştur
 const createBadge = async (req, res) => {
@@ -13,7 +13,14 @@ const createBadge = async (req, res) => {
         });
 
         const savedBadge = await newBadge.save();
-        res.status(201).json(savedBadge);
+        const populatedBadge = await Badge.findById(savedBadge._id).populate('icon', 'filePath');
+
+        const response = {
+            ...populatedBadge.toObject(),
+            filename: populatedBadge.icon?.filePath ? populatedBadge.icon.filePath.split('/').pop() : null,
+        };
+
+        res.status(201).json(response);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
@@ -22,8 +29,13 @@ const createBadge = async (req, res) => {
 // Tüm rozetleri listele
 const getBadges = async (req, res) => {
     try {
-        const badges = await Badge.find();
-        res.status(200).json(badges);
+        const badges = await Badge.find().populate('icon', 'filePath');
+        const response = badges.map(badge => ({
+            ...badge.toObject(),
+            filename: badge.icon?.filePath ? badge.icon.filePath.split('/').pop() : null,
+        }));
+
+        res.status(200).json(response);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
@@ -34,12 +46,17 @@ const getBadgeById = async (req, res) => {
     try {
         const { id } = req.params;
 
-        const badge = await Badge.findById(id);
+        const badge = await Badge.findById(id).populate('icon', 'filePath');
         if (!badge) {
             return res.status(404).json({ message: 'Badge not found' });
         }
 
-        res.status(200).json(badge);
+        const response = {
+            ...badge.toObject(),
+            filename: badge.icon?.filePath ? badge.icon.filePath.split('/').pop() : null,
+        };
+
+        res.status(200).json(response);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
@@ -51,12 +68,17 @@ const updateBadge = async (req, res) => {
         const { id } = req.params;
         const updates = req.body;
 
-        const updatedBadge = await Badge.findByIdAndUpdate(id, updates, { new: true });
+        const updatedBadge = await Badge.findByIdAndUpdate(id, updates, { new: true }).populate('icon', 'filePath');
         if (!updatedBadge) {
             return res.status(404).json({ message: 'Badge not found' });
         }
 
-        res.status(200).json(updatedBadge);
+        const response = {
+            ...updatedBadge.toObject(),
+            filename: updatedBadge.icon?.filePath ? updatedBadge.icon.filePath.split('/').pop() : null,
+        };
+
+        res.status(200).json(response);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
